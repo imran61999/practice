ea85e77649c5f0c97150a1ebd0d4b3be12172519
imran61999 <smemrannit@gmail.com>
2024-02-23 11:03:06 +0600

    defult value set of function prarameter

---
+++ js/defult.js
@@ -0,0 +1,16 @@
+// function add(num1, num2){
+//     const sum = num1 + num2 ;
+//     console.log(sum);
+//     return sum ;
+// }
+
+
+// add(8 , 8);
+function add(num1=0 , num2=0){
+    const sum = num1 + num2 ;
+    console.log(sum);
+    return sum ;
+}
+
+
+add( );
