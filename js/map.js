bf75c64737bc02dfd29186ab3a690d0e3f05ce59
imran61999 <smemrannit@gmail.com>
2024-02-24 16:30:42 +0600

    added arrow function

--- js/map.js
+++ js/map.js
@@ -1,9 +1,21 @@
-const numbers = [2, 4, 6, 8, 10];
-const result = [];
+// const numbers = [2, 4, 6, 8, 10];
+// const result = [];
 
-for(const number of numbers){
-    const count= number *2 ;
-    result.push(count);
+// for(const number of numbers){
+//     const count= number *2 ;
+//     result.push(count);
+// }
+
+// console.log(result);
+
+const numbers = [2, 4, 6, 8, 10];
+
+function doubleIt(num){
+    return num*2;
 }
 
-console.log(result);
+const result = numbers.map(doubleIt);
+// console.log(result);
+
+// arrow function
+const defult = numbers.map(n => n*2);
