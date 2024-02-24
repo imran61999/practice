17c98bcfa974927567d4d0859e316c2681b876f5
imran61999 <smemrannit@gmail.com>
2024-02-24 20:33:18 +0600

    added find, filter function

---
+++ js/filter.js
@@ -0,0 +1,7 @@
+const name =['adib', 'rasel', 'hossain', 'morad', 'ahnaf'];
+const result = name.filter(n => n.length > 2);
+console.log(result);
+
+const number =[34, 56, 23, 30, 3, 6, 12, 78, 46, 23, 567];
+const count=number.filter(num => num > 20);
+console.log(count);
---
+++ js/find.js
@@ -0,0 +1,3 @@
+const numbers = [345, 56, 78, 35, 67, 45, 89, 35, 90] ;
+const result = numbers.find(n => n < 20);
+console.log(result);
