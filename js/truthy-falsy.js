111b30bb2e52f4dc14ee0780162ce62845a02ccc
imran61999 <smemrannit@gmail.com>
2024-02-25 20:59:19 +0600

    chack truthy and falsy

---
+++ js/truthy-falsy.js
@@ -0,0 +1,29 @@
+
+// some falsy type
+// -------------------------------
+
+// const value = "";
+// console.log(!!value);
+
+// const value = null;
+// console.log(!!value);
+
+// const value = NaN;
+// console.log(!!value);
+
+// const value = undefined;
+// console.log(!!value);
+
+// const value = 0;
+// console.log(!!value);
+
+// some truthy type
+
+// const value = "0";
+// console.log(!!value);
+
+// const value = [];
+// console.log(!!value);
+
+const value = true;
+console.log(!!value);
