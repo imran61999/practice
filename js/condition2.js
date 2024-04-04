cf1ebeea55836351deb08eda9698bec036c12882
imran61999 <smemrannit@gmail.com>
2024-04-04 15:09:03 +0600

    display result

---
+++ js/condition2.js
@@ -0,0 +1,11 @@
+const num = 67;
+
+if(num > 100){
+    console.log("You are wonderful result ");
+}
+else if(num> 50 && num <80){
+    console.log('You are good result result');
+}
+else{
+    console.log('Sorry your result is very bad');
+}
